55febfc2050823b90b48ca9eae199d0fa4aec06a
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-07 12:28:04 +0500

    created the frame reference

--- src/frameReference.m
+++ src/frameReference.m
@@ -1,1 +1,7 @@
-a=disp("will add frameReference code here")
+figure
+
+line([0,0],[0,1],[0,0],'Linewidth',[1],'Color','r');
+line([0,1],[0,0],[0,0],'Linewidth',[1.5],'Color','g');
+line([0,0],[0,0],[0,1],'Linewidth',[2],'Color','b');
+%specifying an offset for drawing frame of reference
+axis([-3 3 -3 3]);
