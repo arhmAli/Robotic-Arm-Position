671383dbd0b658b36097501fa935d4496a72c553
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-07 12:27:42 +0500

    created a frame of reference

--- src/axisFixation.m
+++ src/axisFixation.m
@@ -1,2 +1,9 @@
-%will add axis file
-a=10;
+%Specifying a frame of reference
+figure
+
+line([0,0],[0,1],[0,0],'Linewidth',[1],'Color','r');
+line([0,1],[0,0],[0,0],'Linewidth',[1.5],'Color','g');
+line([0,0],[0,0],[0,1],'Linewidth',[2],'Color','b');
+%specifying an offset for drawing frame of reference
+axis([-3 3 -3 3]);
+
